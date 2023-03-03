f1c8e29e33c4fc1549fdc83faae848f004211fc3
Mark Ricard <ricard@uta.edu>
2023-03-03 11:22:13 -0600

    Update Analysis_Main.py

--- Analysis_Main.py
+++ Analysis_Main.py
@@ -2,13 +2,15 @@ from Visco import *
 
 
 if __name__ == '__main__':
-    file_name = 'd:/Loran Stiffness/S0 C0 80% T1.csv'
+    file_name = 'D:/Loran Stiffness/S3 C0 100%  T2.csv'
     p1 = Visco(file_name)
+    p1.graph_residual()
     #p1.plot_all()
-    p1.filter_data()
-    p1.find_rep(False)  # change to True to view start, peak torque, end for each rep
-    p1.analyze_reps()
+    #p1.filter_data()
+    #p1.find_rep(False)  # change to True to view start, peak torque, end for each rep
+    #p1.analyze_reps()
 
-    #p1.graph_rep(2)
-    p1.print_results()
-    p1.graph_all_reps()
+    #p1.graph_rep(5)
+    #p1.save_rep(5, 'F:/Loran Stiffness/end cushion/S3 C0 100%  T2.csv')
+    #p1.print_results()
+    #p1.graph_all_reps()
